a4b6a7942866e11c836e7284b5639a5220a78fa0
Nontapon Tuvinan <npersec@gmail.com>
2026-06-02 00:22:07 +0700

    chore: set favicon to SuperWin logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "SUPERWIN HUB",
-  description: "PUBG MOBILE prediction room using free coins."
+  description: "PUBG MOBILE prediction room using free coins.",
+  icons: {
+    icon: "/SuperWin_b.png",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
