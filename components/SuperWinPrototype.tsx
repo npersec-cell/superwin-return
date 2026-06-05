8e70fb4caf2dc5298fa10592cf230a85e152d79b
Nontapon Tuvinan <npersec@gmail.com>
2026-06-05 12:39:31 +0700

    Update SuperWinPrototype.tsx

--- components/SuperWinPrototype.tsx
+++ components/SuperWinPrototype.tsx
@@ -718,18 +718,11 @@ export default function SuperWinPrototype() {
       closesAt: item.closesAt,
       totalPool: item.totalPool,
       playerCount: item.playerCount,
-      options: (() => {
-        const shuffled = item.options.slice();
-        for (let i = shuffled.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-        }
-        return shuffled.map((option) => ({
-          id: option.id,
-          name: option.label,
-          returns: option.estimatedReturnPercent
-        }));
-      })()
+      options: item.options.map((option) => ({
+        id: option.id,
+        name: option.label,
+        returns: option.estimatedReturnPercent
+      }))
     }));
 
     setLiveQuestions(apiQuestions);
